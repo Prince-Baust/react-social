5ff42d8e0195fdacb5f582cb2208809126288c8b
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-10-06 01:15:28 +0600

    states declared

---
+++ src/components/Facebook.js
@@ -0,0 +1,15 @@
+import FacebookLogin from 'react-facebook-login';
+import {useState} from "react";
+
+const Facebook = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userID, setUserID] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [picture, setPicture] = useState('');
+
+
+  return
+}
+
+export default Facebook;
